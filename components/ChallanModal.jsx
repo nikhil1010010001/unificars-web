83c06f0efbb1fe507a6e44f851eb9d63c9f83f99
Nikhil <nikhil1010010001@gmail.com>
2024-07-03 17:03:15 +0530

    topbar update

---
+++ components/ChallanModal.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ChallanModal = () => {
+  return <div>ChallanModal</div>;
+};
+
+export default ChallanModal;
--- components/TopBar.jsx
+++ components/TopBar.jsx
@@ -1,61 +1,27 @@
-"use client";
-
 import { Mail, PhoneCall } from "@/common/IconsSvg";
 import Link from "next/link";
-import React, { useEffect, useState } from "react";
-import Marquee from "react-fast-marquee";
 
 function TopBar() {
-  const [isDesktop, setIsDesktop] = useState(false);
-
-  useEffect(() => {
-    const handleResize = () => {
-      setIsDesktop(window.innerWidth >= 768);
-    };
-    window.addEventListener("resize", handleResize);
-    handleResize();
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-
-  if (isDesktop) {
-    return (
-      <div className="bg-orange-500 text-white px-4 lg:px-28 py-2 flex  justify-end items-center gap-4 md:gap-8 sticky top-0 z-20">
+  return (
+    <div className="w-full bg-orange-500 text-white sticky top-0 z-20">
+      <div className="overflow-x-auto w-full px-2 lg:px-28 py-2 flex justify-between items-center gap-4 md:gap-8 text-sm md:text-base">
         <p className="flex items-center gap-2">
-          <PhoneCall />
+          <PhoneCall className="md:w-6 w-4" />
           <span className="md:block hidden">Toll Free : </span>
-          <span>+91-9911771977</span>
+          <span className="whitespace-nowrap">+91-9911771977</span>
         </p>
-        <Link
-          href="mailto:info@unificars.com"
-          className="flex items-center gap-2">
-          <Mail />
-          <span>info@unificars.com</span>
-        </Link>
-      </div>
-    );
-  } else {
-    return (
-      <Marquee
-        speed={50}
-        gradient={false}
-        pauseOnHover={true}
-        className="bg-orange-500">
-        <div className="w-screen mx-auto text-white px-4 lg:px-28 py-2 flex justify-evenly md:justify-end items-center gap-8 md:gap-8 sticky top-0 z-20">
-          <p className="flex items-center gap-2">
-            <PhoneCall />
-            <span className="md:block hidden">Toll Free : </span>
-            <span className="whitespace-nowrap">+91-9911771977</span>
-          </p>
+
+        <div>
           <Link
             href="mailto:info@unificars.com"
             className="flex items-center gap-2">
-            <Mail />
+            <Mail className="md:w-6 w-4" />
             <span>info@unificars.com</span>
           </Link>
         </div>
-      </Marquee>
-    );
-  }
+      </div>
+    </div>
+  );
 }
 
 export default TopBar;
